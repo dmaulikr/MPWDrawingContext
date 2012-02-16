55c7babc559ce173a505dedfbb9cacd0decaed53
Marcel Weiher <marcel@metaobject.com>
2012-02-16 12:25:29 +0100

    revert Objective-C code to original, non-context

--- IconView.m
+++ IconView.m
@@ -96,106 +96,76 @@
 	CGFloat nativeAspect = nativeSize.width / nativeSize.height;
 	CGFloat boundsAspect = boundsSize.width / boundsSize.height;
 	CGFloat scale = nativeAspect > boundsAspect ?
-		boundsSize.width / nativeSize.width :
-		boundsSize.height / nativeSize.height;
-
-	[context gsave];
-    [[context translate:0.5 * (boundsSize.width - scale * nativeSize.width) :0.5 * (boundsSize.height - scale * nativeSize.height)] 
-     scale:scale :scale];
+    boundsSize.width / nativeSize.width :
+    boundsSize.height / nativeSize.height;
+	
+	[[NSGraphicsContext currentContext] saveGraphicsState];
     
+	NSAffineTransform *transform = [[NSAffineTransform alloc] init];
+	[transform translateXBy:0.5 * (boundsSize.width - scale * nativeSize.width) yBy:0.5 * (boundsSize.height - scale * nativeSize.height)];
+	[transform scaleBy:scale];
+	[transform set];
 	
 	NSRect ellipseRect = NSMakeRect(32, 38, 448, 448);
 	
 	[NSShadow setShadowWithOffset:NSMakeSize(0, -8 * scale) blurRadius:12 * scale color:[NSColor colorWithCalibratedWhite:0 alpha:0.75]];
-    [context setFillColorGray:0.9 alpha:1.0];
-    
-//	[[NSColor colorWithCalibratedWhite:0.9 alpha:1.0] set];
-
-	[[context ellipseInRect:ellipseRect] fill];
-
+	[[NSColor colorWithCalibratedWhite:0.9 alpha:1.0] set];
+	[[NSBezierPath bezierPathWithOvalInRect:ellipseRect] fill];
 	[NSShadow clearShadow];
-
-//	NSBezierPath *ellipse = [NSBezierPath bezierPathWithOvalInRect:ellipseRect];
-
-    [[context ellipseInRect:ellipseRect] clip];
-	NSPoint endPoint = NSMakePoint(ellipseRect.origin.x, ellipseRect.origin.y + ellipseRect.size.height);
-
-    [context drawLinearGradientFrom:ellipseRect.origin
-                                 to:endPoint
-                             colors:[NSArray arrayWithObjects:
-                                     [context colorGray:1.0 alpha:1.0],
-                                     [context colorGray:0.82 alpha:1.0],
-                                     nil]
-                            offsets:[MPWRealArray arrayWithReals:(float[]){1.0,0.0} count:2]];
-
     
-    NSGradient *borderGradient =
-		[[[NSGradient alloc]
-			initWithStartingColor:[NSColor colorWithCalibratedWhite:1.0 alpha:1.0]
-			endingColor:[NSColor colorWithCalibratedWhite:0.82 alpha:1.0]]
-		autorelease];
-
-//	[borderGradient drawInBezierPath:ellipse angle:-90];
-
-	NSRect ellipseCenterRect = NSInsetRect(ellipseRect, 16, 16);
-    NSLog(@"ellipseCenterRect: %@",NSStringFromRect(ellipseCenterRect));
+	NSBezierPath *ellipse = [NSBezierPath bezierPathWithOvalInRect:ellipseRect];
+	NSGradient *borderGradient =
+    [[[NSGradient alloc]
+      initWithStartingColor:[NSColor colorWithCalibratedWhite:1.0 alpha:1.0]
+      endingColor:[NSColor colorWithCalibratedWhite:0.82 alpha:1.0]]
+     autorelease];
+	[borderGradient drawInBezierPath:ellipse angle:-90];
 	
-	[[[context setFillColorGray:0.0 alpha:1.0] ellipseInRect:ellipseCenterRect] fill];
-
-    [[context ellipseInRect:ellipseCenterRect] clip];
-
-    NSPoint centerPoint = NSMakePoint( NSMidX( ellipseCenterRect ), NSMidY(ellipseCenterRect) - 0.1 * ellipseCenterRect.size.height);
-
-    NSLog(@"first glow centerPoint: %@",NSStringFromPoint(centerPoint));
-    // bottom glow gradient 
-    [context drawRadialGradientFrom:centerPoint radius:0.0
-                                 to:centerPoint radius:0.8 * ellipseCenterRect.size.height 
-                             colors:[NSArray arrayWithObjects:
-                                     [context colorRed:0 green:0.94 blue:0.82 alpha:1.0],
-                                     [context colorRed:0 green:0.62 blue:0.56 alpha:1.0],
-                                     [context colorRed:0 green:0.05 blue:0.35 alpha:1.0],
-                                     [context colorRed:0 green:0.0 blue:0.0 alpha:1.0],
-
-                                     nil]
-                            offsets: [MPWRealArray arrayWithReals:(float[]){0.0,0.35,0.6,0.7} count:4]];
-
-    centerPoint = NSMakePoint( NSMidX( ellipseCenterRect ), NSMidY(ellipseCenterRect) + 0.4 * ellipseCenterRect.size.height);
-    NSLog(@"second glow centerPoint: %@",NSStringFromPoint(centerPoint));
-    [context drawRadialGradientFrom:centerPoint radius:0.0
-                                 to:centerPoint radius:0.8 * ellipseCenterRect.size.height 
-                             colors:[NSArray arrayWithObjects:
-                                     [context colorRed:0 green:0.68 blue:1.0 alpha:0.75],
-                                     [context colorRed:0 green:0.45 blue:0.62 alpha:0.55],
-                                     [context colorRed:0 green:0.45 blue:0.62 alpha:0.0],
-                                     
-                                     nil]
-                            offsets:[MPWRealArray arrayWithReals:(float[]){0.0,0.25,0.4} count:3]];
- 
- 
-    centerPoint = NSMakePoint( NSMidX( ellipseCenterRect ), NSMidY(ellipseCenterRect) );
-    NSLog(@"third glow centerPoint: %@",NSStringFromPoint(centerPoint));
-    [context drawRadialGradientFrom:centerPoint radius:0.0
-                                 to:centerPoint radius:0.8 * ellipseCenterRect.size.height 
-                             colors:[NSArray arrayWithObjects:
-                                     [context colorRed:0 green:0.9 blue:0.9 alpha:0.9],
-                                     [context colorRed:0 green:0.49 blue:1.0 alpha:0.0],                                     
-                                     nil]
-                            offsets:[NSArray arrayWithObjects:
-                                     [NSNumber numberWithFloat:0.0],
-                                     [NSNumber numberWithFloat:0.85],
-                                     nil ]];
-#if 1    
-	NSFont *arialUnicode =
-		[[NSFontManager sharedFontManager]
-			fontWithFamily:@"Arial Unicode MS"
-			traits:0
-			weight:5
-			size:345];
-
-	NSLog(@"NSFontAttributeName: '%@'",NSFontAttributeName);
-	NSLog(@"NSForegroundColorAttributeName: '%@'",NSForegroundColorAttributeName);
+	NSRect ellipseCenterRect = NSInsetRect(ellipseRect, 16, 16);
+	[[NSColor blackColor] set];
+	NSBezierPath *ellipseCenter = [NSBezierPath bezierPathWithOvalInRect:ellipseCenterRect];
+	[ellipseCenter fill];
+	
+	[ellipseCenter setClip];
     
-    // Getting the glyph using AppKit's NSLayoutManager
+	NSGradient *bottomGlowGradient =
+    [[[NSGradient alloc]
+      initWithColorsAndLocations:
+      [NSColor colorWithCalibratedRed:0 green:0.94 blue:0.82 alpha:1.0], 0.0,
+      [NSColor colorWithCalibratedRed:0 green:0.62 blue:0.56 alpha:1.0], 0.35,
+      [NSColor colorWithCalibratedRed:0 green:0.05 blue:0.35 alpha:1.0], 0.6,
+      [NSColor colorWithCalibratedRed:0 green:0.0 blue:0.0 alpha:1.0], 0.7,
+      nil]
+     autorelease];
+	[bottomGlowGradient drawInRect:ellipseCenterRect relativeCenterPosition:NSMakePoint(0, -0.2)];
+    
+	NSGradient *topGlowGradient =
+    [[[NSGradient alloc]
+      initWithColorsAndLocations:
+      [NSColor colorWithCalibratedRed:0 green:0.68 blue:1.0 alpha:0.75], 0.0,
+      [NSColor colorWithCalibratedRed:0 green:0.45 blue:0.62 alpha:0.55], 0.25,
+      [NSColor colorWithCalibratedRed:0 green:0.45 blue:0.62 alpha:0.0], 0.40,
+      nil]
+     autorelease];
+	[topGlowGradient drawInRect:ellipseCenterRect relativeCenterPosition:NSMakePoint(0, 0.4)];
+    
+	NSGradient *centerGlowGradient =
+    [[[NSGradient alloc]
+      initWithColorsAndLocations:
+      [NSColor colorWithCalibratedRed:0 green:0.9 blue:0.9 alpha:0.9], 0.0,
+      [NSColor colorWithCalibratedRed:0 green:0.49 blue:1.0 alpha:0.0], 0.85,
+      nil]
+     autorelease];
+	[centerGlowGradient drawInRect:ellipseCenterRect relativeCenterPosition:NSMakePoint(0, 0)];
+    
+	NSFont *arialUnicode =
+    [[NSFontManager sharedFontManager]
+     fontWithFamily:@"Arial Unicode MS"
+     traits:0
+     weight:5
+     size:345];
+    
+	// Getting the glyph using AppKit's NSLayoutManager
 	NSString *floralHeart = @"\u2766";
 	NSRange stringRange = NSMakeRange(0, [floralHeart length]);
 	NSLayoutManager *layoutManager = [[[NSLayoutManager alloc] init] autorelease];
@@ -207,89 +177,64 @@
 	NSGlyph *glyphs = (NSGlyph *)malloc(sizeof(NSGlyph) * (numGlyphs + 1)); // includes space for NULL terminator
 	[layoutManager getGlyphs:glyphs range:NSMakeRange(0, numGlyphs)];
 	[textStorage removeLayoutManager:layoutManager];
-
-//	// Get the glyph using CTFont instead
-//	NSInteger numGlyphs = 1;
-//	NSGlyph *glyphs = (NSGlyph *)malloc(sizeof(NSGlyph) * (numGlyphs + 1)); // includes space for NULL terminator
-//	CTFontGetGlyphsForCharacters((CTFontRef)arialUnicode, (const UniChar *)L"\u2766", (CGGlyph *)glyphs, numGlyphs);
+    
+    //	// Get the glyph using CTFont instead
+    //	NSInteger numGlyphs = 1;
+    //	NSGlyph *glyphs = (NSGlyph *)malloc(sizeof(NSGlyph) * (numGlyphs + 1)); // includes space for NULL terminator
+    //	CTFontGetGlyphsForCharacters((CTFontRef)arialUnicode, (const UniChar *)L"\u2766", (CGGlyph *)glyphs, numGlyphs);
 	
 	NSBezierPath *floralHeartPath = [[[NSBezierPath alloc] init] autorelease];
 	[floralHeartPath moveToPoint:NSMakePoint(130, 140)];
 	[floralHeartPath appendBezierPathWithGlyphs:glyphs count:numGlyphs inFont:arialUnicode];
 	free(glyphs);
-
+    
 	[NSShadow setShadowWithOffset:NSZeroSize blurRadius:12 * scale color:[NSColor colorWithCalibratedWhite:0 alpha:1.0]];
 	[[NSColor colorWithCalibratedWhite:0.9 alpha:1.0] set];
 	[floralHeartPath fill];
 	[borderGradient drawInBezierPath:floralHeartPath angle:-90];
-    
-    
 	[NSShadow clearShadow];
-#else
-    [[context setTextPosition:NSMakePoint(0, 0)] setFillColor:[context colorGray:0.9 alpha:1.0]];
-    [[[NSFontManager sharedFontManager]
-     fontWithFamily:@"Arial Unicode MS"
-     traits:0
-     weight:5
-     size:45] set];
-
-//    [context selectMacRomanFontName:@"Arial Unicode MS" size:345];
-    [context show:@"\u2766"];
-//    [context show:@"&"];
-
-#endif
+    
 	const CGFloat glossInset = 8;
 	CGFloat glossRadius = (ellipseCenterRect.size.width * 0.5) - glossInset;
 	NSPoint center = NSMakePoint(NSMidX(ellipseRect), NSMidY(ellipseRect));
-    NSLog(@"arc center: %@ from rect: %@",NSStringFromPoint(center),NSStringFromRect(ellipseRect));
-
+    
 	double arcFraction = 0.02;
 	NSPoint arcStartPoint = NSMakePoint(
-		center.x - glossRadius * cos(arcFraction * M_PI),
-		center.y + glossRadius * sin(arcFraction * M_PI));
+                                        center.x - glossRadius * cos(arcFraction * M_PI),
+                                        center.y + glossRadius * sin(arcFraction * M_PI));
 	NSPoint arcEndPoint = NSMakePoint(
-		center.x + glossRadius * cos(arcFraction * M_PI),
-		center.y + glossRadius * sin(arcFraction * M_PI));
-
-    NSLog(@"arcStartPoint: %@",NSStringFromPoint(arcStartPoint));
-    NSLog(@"arcEndPoint: %@",NSStringFromPoint(arcEndPoint));
-    NSLog(@"arcFraction (%g) cos:%g sin:%g",arcFraction,cos(arcFraction * M_PI),sin(arcFraction * M_PI));
+                                      center.x + glossRadius * cos(arcFraction * M_PI),
+                                      center.y + glossRadius * sin(arcFraction * M_PI));
     
 	NSBezierPath *glossPath = [[[NSBezierPath alloc] init] autorelease];
 	[glossPath moveToPoint:arcStartPoint];
 	[glossPath
-		appendBezierPathWithArcWithCenter:center
-		radius:glossRadius
-		startAngle:arcFraction * 180
-		endAngle:(1.0 - arcFraction) * 180];
-
+     appendBezierPathWithArcWithCenter:center
+     radius:glossRadius
+     startAngle:arcFraction * 180
+     endAngle:(1.0 - arcFraction) * 180];
+    
 	const CGFloat bottomArcBulgeDistance = 70;
 	const CGFloat bottomArcRadius = 2.6;
 	[glossPath moveToPoint:arcEndPoint];
 	[glossPath
-		appendBezierPathWithArcFromPoint:
-			NSMakePoint(center.x, center.y - bottomArcBulgeDistance)
-		toPoint:arcStartPoint
-		radius:glossRadius * bottomArcRadius];
+     appendBezierPathWithArcFromPoint:
+     NSMakePoint(center.x, center.y - bottomArcBulgeDistance)
+     toPoint:arcStartPoint
+     radius:glossRadius * bottomArcRadius];
 	[glossPath lineToPoint:arcStartPoint];
-
+    
 	NSGradient *glossGradient =
-		[[[NSGradient alloc]
-			initWithColorsAndLocations:
-				[NSColor colorWithCalibratedWhite:1 alpha:0.85], 0.0,
-				[NSColor colorWithCalibratedWhite:1 alpha:0.5], 0.5,
-				[NSColor colorWithCalibratedWhite:1 alpha:0.05], 1.0,
-			nil]
-		autorelease];
-#if 0   
-    [glossPath setClip];
-    NSRect glossRect =ellipseCenterRect;
-    glossRect.size.height *= 0.5;
-    [glossGradient drawInRect:glossRect angle:-90];
-#else
+    [[[NSGradient alloc]
+      initWithColorsAndLocations:
+      [NSColor colorWithCalibratedWhite:1 alpha:0.85], 0.0,
+      [NSColor colorWithCalibratedWhite:1 alpha:0.5], 0.5,
+      [NSColor colorWithCalibratedWhite:1 alpha:0.05], 1.0,
+      nil]
+     autorelease];
 	[glossGradient drawInBezierPath:glossPath angle:-90];
-#endif
-    [context grestore];
+    
+	[[NSGraphicsContext currentContext] restoreGraphicsState];
 }
 
 @end
